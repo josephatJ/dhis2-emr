3b0eecd97773f92b732bf2b0558af718fa0dd724
mdachi-codes <jessejustinm@gmail.com>
2020-04-24 08:05:03 +0300

    style: style chart and indicator loaders

--- src/app/core/helpers/htmlHelpers.ts
+++ src/app/core/helpers/htmlHelpers.ts
@@ -12,8 +12,13 @@ export function getElementDetails() {
     elementObject['category'] = element.getAttribute('category');
 
     elementsArray.push(elementObject);
-
-    document.getElementById(element.id).innerHTML = 'loading';
+    if (element.getAttribute('type') == 'indicator') {
+      document.getElementById(element.id).innerHTML =
+        '<span style="background-color:#ccc;padding: 4px;">loading indicator<div class="spinner-grow text-primary" role="status"><span class="sr-only">Loading...</span></div></span>';
+    } else if (element.getAttribute('type') == 'charts') {
+      document.getElementById(element.id).innerHTML =
+        '<div style="margin-top:8px;margin-bottom:12px;width:100%;height:250px;background-color:#ccc;text-align:center"><p style="padding:5px;">loading chart</p><div class="spinner-grow text-primary" role="status"><span class="sr-only">Loading...</span></div></div>';
+    }
   });
 
   return elementsArray;
