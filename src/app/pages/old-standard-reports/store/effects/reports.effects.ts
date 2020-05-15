9caad362891143ca10f01de412f973018ffe48fd
josephatJ <josephatjulius24@gmail.com>
2020-05-15 10:28:10 +0300

    perf: add logic to check if data to load are available on store

--- src/app/pages/old-standard-reports/store/effects/reports.effects.ts
+++ src/app/pages/old-standard-reports/store/effects/reports.effects.ts
@@ -9,48 +9,73 @@ import {
   addLoadedReportMetadata,
   loadingReportMetadataFails
 } from '../actions';
-import { switchMap, map, catchError, mergeMap } from 'rxjs/operators';
+import {
+  switchMap,
+  map,
+  catchError,
+  mergeMap,
+  withLatestFrom
+} from 'rxjs/operators';
 import { of, from } from 'rxjs';
 import { sanitizeLoadedReportsTypes } from '../../helpers/sanitize-loaded-reports-types.helper';
+import { Store } from '@ngrx/store';
+import { State } from 'src/app/store/reducers';
+import {
+  getOldReportsMetadataEntities,
+  getCountOfLoadedReportTypes
+} from '../selectors';
 
 @Injectable()
 export class OldReportsEffects {
   reportsList$ = createEffect(() =>
     this.actions$.pipe(
       ofType(loadStdReportsList),
-      switchMap(action =>
-        from(action.reportsTypes).pipe(
-          mergeMap((reportType: any) =>
-            this.reportsService.loadReportsList(reportType).pipe(
-              map(reportsList =>
-                addLoadedStdReportsList({
-                  reportsList: sanitizeLoadedReportsTypes(reportsList)
-                })
-              ),
-              catchError(error => of(loadingOldStdReportsListFails({ error })))
+      withLatestFrom(this.store.select(getCountOfLoadedReportTypes)),
+      switchMap(([action, countOfReportTypes]) => {
+        if (countOfReportTypes == 2) {
+          return from([]);
+        } else {
+          return from(action.reportsTypes).pipe(
+            mergeMap((reportType: any) =>
+              this.reportsService.loadReportsList(reportType).pipe(
+                map(reportsList =>
+                  addLoadedStdReportsList({
+                    reportsList: sanitizeLoadedReportsTypes(reportsList)
+                  })
+                ),
+                catchError(error =>
+                  of(loadingOldStdReportsListFails({ error }))
+                )
+              )
             )
-          )
-        )
-      )
+          );
+        }
+      })
     )
   );
 
   reportMetadata$c = createEffect(() =>
     this.actions$.pipe(
       ofType(loadReportMetadata),
-      switchMap(action =>
-        this.reportsService
-          .loadReportContents(action.reportId, action.reportType)
-          .pipe(
-            map(report => addLoadedReportMetadata({ report })),
-            catchError(error => of(loadingReportMetadataFails({ error })))
-          )
-      )
+      withLatestFrom(this.store.select(getOldReportsMetadataEntities)),
+      switchMap(([action, reportMetadataEntitiess]: [any, any]) => {
+        if (reportMetadataEntitiess[action.reportId]) {
+          return from([]);
+        } else {
+          return this.reportsService
+            .loadReportContents(action.reportId, action.reportType)
+            .pipe(
+              map(report => addLoadedReportMetadata({ report })),
+              catchError(error => of(loadingReportMetadataFails({ error })))
+            );
+        }
+      })
     )
   );
 
   constructor(
     private actions$: Actions,
-    private reportsService: OldReportsService
+    private reportsService: OldReportsService,
+    private store: Store<State>
   ) {}
 }
